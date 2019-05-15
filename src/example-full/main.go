6e2e07a85022cf38bb34ccdf63691ba6d089f956
yellowmegaman <yellowmegaman@gmail.com>
2019-05-16 00:40:44 +0300

    test db and check

--- src/example-full/main.go
+++ src/example-full/main.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+var (
+	host     = os.Getenv("PGHOST")
+	user     = os.Getenv("PGUSER")
+	password = os.Getenv("PGPASS")
+	dbname   = os.Getenv("PGDBNAME")
+)
 
 
 func main() {
-	fmt.Println("hi!")
+	connectdb() // let's start with that, since if db isn't available, why bother serving http endpoint
+	apiserve()
+}
+
+func connectdb() {
+	pgconnection := fmt.Sprintf("host=%s port=%d user=%s "+ "password=%s dbname=%s sslmode=disable", host, 5432, user, password, dbname)
+	db, err := sql.Open("postgres", pgconnection)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected to db!")
+}
+
+
+func apiserve() {
+	
 }
